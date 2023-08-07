6b8e6d6a1578504b798c380e2a9d5b6b76edc7ac
TrevorReznick <135354265+TrevorReznick@users.noreply.github.com>
2023-08-07 13:56:25 +0200

    Adding project to store

--- store/index.js
+++ store/index.js
@@ -1,4 +1,15 @@
-export const state = () => ({})
+export const state = () => ({
+  projects: [
+    {
+      name: 'project_1',
+      title: 'My dummy project',
+      short_text: 'This is a short text',
+      text: 'This is long text, This is long text, This is long text, This is long text, This is long text, This is long text',
+      tech_stack: [],
+      tags: []
+    }
+  ]
+})
 
 export const getters = {
   getProjectById: (state) => (id) => {
