2110b6baa180f57149f8da6e4056ca3f886c09b9
TrevorReznick <135354265+TrevorReznick@users.noreply.github.com>
2023-08-07 10:47:34 +0200

    Updated store/index.js

--- store/index.js
+++ store/index.js
@@ -1,1 +1,15 @@
+export const state = () => ({})
 
+export const getters = {
+  getProjectById: (state) => (id) => {
+    return state.projects.find((project) => project.id == id)
+  }
+}
+
+export const mutations = {
+  // @todo
+}
+
+export const actions = {
+  // @todo
+}
